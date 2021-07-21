2113ce7cd1aaf42e979c414a27097b4b3d839e9f
RAUL PETRU METESAN <raul.metesan@s.unibuc.ro>
2021-07-21 23:08:53 +0300

    schimbari

---
+++ app.js
@@ -0,0 +1,25 @@
+const { response } = require('express');
+const express = require('express')
+const app = express()
+const request = require('request');
+const port = 5000
+
+app.get('/', function (req, res) {
+    res.send('hello world')
+  })
+
+app.get('/TemperaturaBucuresti', (req, res) => {
+request('http://api.weatherstack.com/current?access_key=3c8bfd0485a940287d501df2f67394df&query=Bucuresti',
+function(error,response,body) {
+    if(!error && response.statusCode == 200){
+        var ParseT = JSON.parse(body);
+        var temperature = ParseT['current']['temperature']
+    res.send({temperature});
+    }
+})
+})
+
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
